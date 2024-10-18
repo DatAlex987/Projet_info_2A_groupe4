12124db55275a64fc95deace2f201c8e7ca85857
theosannierpro <theosannierpro@gmail.com>
2024-10-18 20:00:25 +0200

    écriture de 1,5 methodes Freesound

--- src/service/freesound.py
+++ src/service/freesound.py
@@ -1,40 +1,106 @@
 # -*- coding: utf-8 -*-
 
-# For housekeeping
+
 from typing import Optional, Dict, Any, Union
 import os
 from dotenv import load_dotenv
-
-# For business code
 import requests
-
-# Loading env var (api key secret, url...)
-load_dotenv()
-URL: Optional[str] = os.getenv("URL_API")
-KEY: Optional[str] = os.getenv("API_KEY")
+from service.singleton import Singleton
 
 
-# Defining the constant headers
-headers = {
-    "Content-type": "application/json",
-}
-payload: Dict[str, Union[str, int]] = {"token": f"{KEY}"}
+class Freesound(metaclass=Singleton):
+    """
+    Classe intéragissant avec l'API Freesound
+    """
 
+    def rechercher_par_tag(tag: str, limit: int):
+        """
+        Envoie une requête à l'API pour récupérer des sons correspondants
+        au tag renseigné.
 
-def request_search(query_term: str) -> Dict:
-    url_search = f"{URL}search/text/"
-    payload_search = payload.copy()
-    payload_search.update({"query": query_term, "limit": 10})
-    req = requests.get(url_search, headers=headers, params=payload_search)
-    results = req.json()
-    return results["results"]
+        Param
+        -----------------
+        tag : str
+            Mot clé pour orienter la recherche
+        limit : int
+            Nombre de sons à renvoyer
 
+        Returns
+        -----------------
+        results : list
+            Liste de longueur l = limit. Chaque élément est un
+            dictionnaire qui contient les informations
+            d'un son : id, nom, tags, licence, username
+        """
+        if not isinstance(tag, str):
+            raise TypeError("L'argument tag n'est pas un str.")
+        if not isinstance(limit, int):
+            raise TypeError("L'argument limit n'est pas un int.")
+        if limit < 0:
+            raise ValueError("L'argument limit ne peut pas être négatif.")
 
-print(request_search("wind"))
-# La sortie est une liste de dico des 10 sons liés au wind (id, name, tags, licence, username)
+        load_dotenv()
+        URL: Optional[str] = os.getenv("URL_API")
+        KEY: Optional[str] = os.getenv("API_KEY")
+        headers = {
+            "Content-type": "application/json",
+        }
+        payload: Dict[str, Union[str, int]] = {"token": f"{KEY}"}
+        url_search = f"{URL}search/text/"
+        payload_search = payload.copy()
+        payload_search.update({"query": tag, "limit": limit})
+        req = requests.get(url_search, headers=headers, params=payload_search)
+        results = req.json()
+        # La sortie est une liste de dico des 10 sons liés au
+        # wind (id, name, tags, licence, username)
+        return results["results"]
 
-
-class Freesound(metaclass=Singleton)
-
-    def recherche_tot():
+    def rechercher_par_nom():
         pass
+
+    def rechercher_par_duree():
+        pass
+
+    def rechercher_par_id():
+        pass
+
+    def rechercher_multi_filtres(dico_filtres: dict, limit: int):  # NOT DONE YET
+        """
+        Envoie une requête à l'API pour récupérer des sons correspondants
+        aux filtres renseignés.
+
+        Param
+        -----------------
+        dico_filtres : dict
+            Dictionnaire dont les clés sont les types de filtres et les valeurs
+            les filtres souhaités (None si filtre non utilisé)
+
+        Returns
+        -----------------
+        results : list
+            Liste de longueur l = limit. Chaque élément est un
+            dictionnaire qui contient les informations
+            d'un son : id, nom, tags, licence, username
+        """
+        if not isinstance(dico_filtres, dict):
+            raise TypeError("L'argument dico_filtres n'est pas un dict.")
+        if not isinstance(limit, int):
+            raise TypeError("L'argument limit n'est pas un int.")
+        if limit < 0:
+            raise ValueError("L'argument limit ne peut pas être négatif.")
+
+        load_dotenv()
+        URL: Optional[str] = os.getenv("URL_API")
+        KEY: Optional[str] = os.getenv("API_KEY")
+        headers = {
+            "Content-type": "application/json",
+        }
+        payload: Dict[str, Union[str, int]] = {"token": f"{KEY}"}
+        url_search = f"{URL}search/text/"
+        payload_search = payload.copy()
+        payload_search.update({"query": tag, "limit": limit})
+        req = requests.get(url_search, headers=headers, params=payload_search)
+        results = req.json()
+        # La sortie est une liste de dico des 10 sons liés au
+        # wind (id, name, tags, licence, username)
+        return results["results"]
---
+++ src/tests/test_service/test_freesound.py
@@ -0,0 +1,30 @@
+from unittest.mock import MagicMock
+import pytest
+import re
+from service.freesound import Freesound
+
+
+@pytest.mark.parametrize(
+    "tag, limit, expected_error, error_type",
+    [
+        (["wind"], 15, "L'argument tag n'est pas un str.", TypeError),
+        ("wind", -4, "L'argument limit ne peut pas être négatif.", ValueError),
+        ("wind", "15", "L'argument limit n'est pas un int.", TypeError),
+    ],
+)
+def test_rechercher_par_tag_echec(tag, limit, expected_error, error_type):
+    with pytest.raises(error_type, match=re.escape(expected_error)):
+        Freesound.rechercher_par_tag(tag, limit)
+
+
+@pytest.mark.parametrize(
+    "dico_filtres, limit, expected_error, error_type",
+    [
+        ("dico", 15, "L'argument dico_filtres n'est pas un dict.", TypeError),
+        ({"wind"}, -4, "L'argument limit ne peut pas être négatif.", ValueError),
+        ({"wind"}, "15", "L'argument limit n'est pas un int.", TypeError),
+    ],
+)
+def test_rechercher_multi_filtres_echec(dico_filtres, limit, expected_error, error_type):
+    with pytest.raises(error_type, match=re.escape(expected_error)):
+        Freesound.rechercher_multi_filtres(dico_filtres, limit)
