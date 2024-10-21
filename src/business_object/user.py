30117c0deb993d166e818bcd8ef640e86b9e9881
BOUZERIA Wissal <id2323@DOMENSAI.ECOLE>
2024-10-21 16:47:12 +0200

    fichier user

--- src/business_object/user.py
+++ src/business_object/user.py
@@ -1,5 +1,7 @@
+import os
 import hashlib
 from datetime import date
+
 
 class User(Personne):
     """
@@ -24,10 +26,10 @@ class User(Personne):
     >>> utilisateur = User("Bocquet", "Noémie", "2003-08-08", "noemie.b", "password123")
     >>> utilisateur.supprimer_utilisateur()
     """
-    
+
     def __init__(self, nom, prenom, date_naissance, id_user, mdp):
         """
-        Initialise un nouvel utilisateur avec les attributs de la classe Personne et ceux spécifiques à un utilisateur.
+        Initialise un nouvel utilisateur avec les attributs de la classe Personne et ceux propre à un utilisateur.
 
         Parameters
         ----------
@@ -42,16 +44,15 @@ class User(Personne):
         mdp : str
             Mot de passe en clair à hacher.
         """
-        super().__init__(nom, prenom, date_naissance)  
+        super().__init__(nom, prenom, date_naissance)
         self.id_user = id_user
-        self.mot_de_passe_hash = self._hash_mot_de_passe(mdp)  
+        self.mot_de_passe_hash = self._hash_mot_de_passe(mdp)
 
     def _hash_mdp(self, mdp):
         mdp_combine = mdp + self.id_user
-        return hashlib.pbkdf2_hmac('sha256', mdp_combine.encode('utf-8'), os.urandom(16), 100000)
+        return hashlib.pbkdf2_hmac("sha256", mdp_combine.encode("utf-8"), os.urandom(16), 100000)
 
     def supprimer_utilisateur(self):
         self.id_user = None
         self.mot_de_passe_hash = None
         print("L'utilisateur a été supprimé avec succès.")
-
