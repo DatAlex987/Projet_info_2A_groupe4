20915a3629c279ab62b9ca347d23ba938a15f9b1
theosannierpro <theosannierpro@gmail.com>
2024-10-22 10:20:57 +0200

    tests fonctionnels

--- theotests.py
+++ theotests.py
@@ -1,17 +1,5 @@
 from src.dao.db_connection import DBConnection
 from src.business_object.user import User
 from src.business_object.personne import Personne
+from src.business_object.scene import Scene
 from datetime import date
-
-
-# test de DBConnection
-def test_db_connection():
-    try:
-        with DBConnection() as conn:
-            print("Connection is open.")
-            # Optionally test a simple query here
-    except Exception as e:
-        print(f"Error: {e}")
-
-
-test_db_connection()
