7155eac829e3c479c847f3d2b18e59ddd9020c6d
theosannierpro <theosannierpro@gmail.com>
2024-10-22 12:02:00 +0200

    ecriture des tests scene

--- theotests.py
+++ theotests.py
@@ -1,5 +1,21 @@
 from src.dao.db_connection import DBConnection
-from src.business_object.user import User
+from business_object.user import User
 from src.business_object.personne import Personne
 from src.business_object.scene import Scene
 from datetime import date
+
+U = User("thierry", "villiers", date(1589, 1, 1), "458749", "mdp", [])
+print(type(U))
+S = Scene(
+    "scenetest",
+    "description",
+    "598687",
+    [],
+    [],
+    [],
+    U,
+    date(1999, 1, 1),
+)
+
+print(S)
+print(S.modifier_nom(12))
