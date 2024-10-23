34838e3249eed4d9ca49b664a1f81706e7c5b930
wissal1971 <wimoam@gmail.com>
2024-10-23 14:14:55 +0200

    merge testdao

--- src/dao/user_dao.py
+++ src/dao/user_dao.py
@@ -6,11 +6,7 @@ from business_object.user import User
 class UserDAO(metaclass=Singleton):
     """Implémente les méthodes du CRUD pour accéder à la base de données des utilisateurs."""
 
-<<<<<<< HEAD
-    def ajouter_user(self, user: User, schema: str) -> int:
-=======
-    def ajouter_user(self, user: User, schema) -> int:
->>>>>>> a509623f61239fccc8d7176a98f523035c11ae99
+    def ajouter_user(self, user: User, schema: str = "projetinfo") -> int:
         """
         Ajoute un utilisateur dans la base de données.
 
@@ -19,14 +15,14 @@ class UserDAO(metaclass=Singleton):
         user : User
             Instance de la classe User contenant les informations de l'utilisateur.
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
 
         Returns
         -------
         int
             L'ID de l'utilisateur ajouté.
         """
-        with DBConnection(schema="SchemaTest").connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 query = f"""
                 INSERT INTO {schema}.utilisateur(id_user, mdp_hashe, date_naissance, nom, prenom)
@@ -46,7 +42,7 @@ class UserDAO(metaclass=Singleton):
                 res = cursor.fetchone()
         return res["id_user"] if res else None
 
-    def supprimer_user(self, id_user: int, schema: str) -> bool:
+    def supprimer_user(self, id_user: int, schema: str = "projetinfo") -> bool:
         """
         Supprime un utilisateur par son ID.
 
@@ -55,14 +51,14 @@ class UserDAO(metaclass=Singleton):
         id_user : int
             L'ID de l'utilisateur à supprimer.
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
 
         Returns
         -------
         bool
             True si la suppression a réussi, sinon False.
         """
-        with DBConnection().connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     f"DELETE FROM {schema}.Utilisateur WHERE id_user = %(id_user)s;",
@@ -70,27 +66,27 @@ class UserDAO(metaclass=Singleton):
                 )
                 return cursor.rowcount > 0
 
-    def consulter_users(self, schema: str) -> list:
+    def consulter_users(self, schema: str = "projetinfo") -> list:
         """
         Récupère la liste de tous les utilisateurs dans la base de données.
 
         Parameters
         ----------
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
 
         Returns
         -------
         list
             Une liste de dictionnaires contenant les informations des utilisateurs.
         """
-        with DBConnection().connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(f"SELECT * FROM {schema}.Utilisateur;")
                 users = cursor.fetchall()
                 return users if users else []
 
-    def rechercher_par_id_user(self, id_user: int, schema: str) -> dict:
+    def rechercher_par_id_user(self, id_user: int, schema: str = "projetinfo") -> dict:
         """
         Recherche un utilisateur dans la base de données par son ID.
 
@@ -99,14 +95,14 @@ class UserDAO(metaclass=Singleton):
         id_user : int
             L'ID de l'utilisateur à rechercher.
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
 
         Returns
         -------
         dict
             Un dictionnaire contenant les informations de l'utilisateur, ou None si aucun utilisateur trouvé.
         """
-        with DBConnection().connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     f"SELECT * FROM {schema}.Utilisateur WHERE id_user = %(id_user)s;",
@@ -115,7 +111,7 @@ class UserDAO(metaclass=Singleton):
                 user = cursor.fetchone()
                 return user if user else None
 
-    def ajouter_sounddeck(self, id_user: int, nom: str, schema: str) -> None:
+    def ajouter_sounddeck(self, id_user: int, nom: str, schema: str = "projetinfo") -> None:
         """
         Ajoute un sounddeck pour un utilisateur.
 
@@ -126,9 +122,9 @@ class UserDAO(metaclass=Singleton):
         nom : str
             Le nom du sounddeck à ajouter.
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
         """
-        with DBConnection().connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     f"INSERT INTO {schema}.Sounddeck(nom, id_user) VALUES (%(nom)s, %(id_user)s);",
@@ -136,7 +132,7 @@ class UserDAO(metaclass=Singleton):
                 )
         print(f"Le sounddeck '{nom}' a été ajouté pour l'utilisateur {id_user}.")
 
-    def consulter_sounddecks_par_user(self, user: User, schema: str) -> list:
+    def consulter_sounddecks_par_user(self, user: User, schema: str = "projetinfo") -> list:
         """
         Récupère tous les sounddecks d'un utilisateur.
 
@@ -145,14 +141,14 @@ class UserDAO(metaclass=Singleton):
         user : User
             Instance de la classe User pour laquelle récupérer les sounddecks.
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
 
         Returns
         -------
         list
             La liste des sounddecks associés à cet utilisateur.
         """
-        with DBConnection().connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     f"SELECT nom FROM {schema}.Sounddeck WHERE id_user = %(id_user)s;",
@@ -161,7 +157,7 @@ class UserDAO(metaclass=Singleton):
                 sounddecks = cursor.fetchall()
                 return [sounddeck["nom"] for sounddeck in sounddecks] if sounddecks else []
 
-    def supprimer_sounddeck(self, id_user: int, nom: str, schema: str) -> bool:
+    def supprimer_sounddeck(self, id_user: int, nom: str, schema: str = "projetinfo") -> bool:
         """
         Supprime un sounddeck d'un utilisateur.
 
@@ -172,14 +168,14 @@ class UserDAO(metaclass=Singleton):
         nom : str
             Le nom du sounddeck à supprimer.
         schema : str
-            Le schéma de la base de données.
+            Le schéma de la base de données. Par défaut, c'est 'projetinfo'.
 
         Returns
         -------
         bool
             True si le sounddeck a été supprimé avec succès, sinon False.
         """
-        with DBConnection().connection as connection:
+        with DBConnection(schema=schema).connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
                     f"DELETE FROM {schema}.Sounddeck WHERE id_user = %(id_user)s AND nom = %(nom)s;",
